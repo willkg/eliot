1e8b8b9bf8894ed185d85f23d32b16fb0d29e915
Will Kahn-Greene <willkg@mozilla.com>
2023-08-01 09:12:45 -0400

    Update requirements
    * black: 23.3.0 -> 23.7.0
    * click: 8.1.3 -> 8.1.6
    * datadog: 0.45.0 -> 0.46.0
    * gunicorn: 20.1.0 -> 21.2.0
    * pip-tools: 6.13.0 -> 7.1.0
    * pytest: 7.3.1 -> 7.4.0
    * requests-mock: 1.10.0 -> 1.11.0
    * sentry-sdk: 1.20.0 -> 1.29.1
    * urllib3: 1.26.15 -> 2.0.4

--- tests/test_cache_manager.py
+++ tests/test_cache_manager.py
@@ -2,11 +2,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import json
 import pathlib
 from unittest.mock import ANY
 
 from everett.manager import ConfigManager, ConfigDictEnv, ConfigOSEnv
+from fillmore.test import diff_event
 from markus.testing import MetricsMock
 import pytest
 
@@ -484,7 +484,12 @@ BROKEN_EVENT = {
             "name": "CPython",
             "version": ANY,
             "build": ANY,
-        }
+        },
+        "trace": {
+            "parent_span_id": None,
+            "span_id": ANY,
+            "trace_id": ANY,
+        },
     },
     "modules": ANY,
     "release": ANY,
@@ -559,11 +564,8 @@ def test_sentry_scrubbing(sentry_helper, cm_client, monkeypatch, tmpdir):
         # Drop the "_meta" bit because we don't want to compare that.
         del event["_meta"]
 
-        # If this test fails, this will print out the new event that you can copy and
-        # paste and then edit above
-        print(json.dumps(event, indent=4, sort_keys=True))
-
-        assert event == BROKEN_EVENT
+        differences = diff_event(event, BROKEN_EVENT)
+        assert differences == []
 
 
 def test_count_sentry_scrub_error():
